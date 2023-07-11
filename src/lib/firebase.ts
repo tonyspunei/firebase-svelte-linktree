dbf546a149ec953b0a22e1c58174d76e4df7f250
Tony Spunei <tony.spunei@yahoo.com>
2023-07-11 17:10:38 +0200

    first commit

---
+++ src/lib/firebase.ts
@@ -0,0 +1,18 @@
+import { initializeApp } from "firebase/app";
+import { getFireStore } from "firebase/firestore";
+import { getAuth } from "firebase/auth";
+import { getStorage } from "firebase/storage";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyC9lHDiM5tw7BABXBHtiI-8QLtE1hZnZSI",
+  authDomain: "fireship-svelte-linktree-96822.firebaseapp.com",
+  projectId: "fireship-svelte-linktree-96822",
+  storageBucket: "fireship-svelte-linktree-96822.appspot.com",
+  messagingSenderId: "327867253565",
+  appId: "1:327867253565:web:805af6d7a96ea390863b54"
+};
+
+export const app = initializeApp(firebaseConfig);
+export const db = getFireStore();
+export const auth = getAuth();
+export const storage = getStorage();

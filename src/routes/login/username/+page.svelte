337d43ed802b8abed7d3c7eb947559d01bf23675
Tony Spunei <tony.spunei@yahoo.com>
2023-07-12 15:34:14 +0200

    add check username availability

--- src/routes/login/username/+page.svelte
+++ src/routes/login/username/+page.svelte
@@ -1,5 +1,46 @@
-<script>
+<script lang="ts">
+  import AuthCheck from "$lib/components/Auth_Check.svelte";
+  import { db, user } from "$lib/firebase";
+  import { doc, getDoc, writeBatch } from "firebase/firestore";
 
+  let username = "";
+  let loading = false;
+  let isAvailable = false;
+
+  let debounceTimer;
+
+  async function checkAvailability () {
+    isAvailable = false;
+    clearTimeout(debounceTimer);
+
+    loading = true;
+
+    debounceTimer = setTimeout(async () => {
+      console.log("checking availability for ", username)
+
+      const ref = doc(db, "usernames", username);
+      const exists = await getDoc(ref).then(doc => doc.exists());
+
+      isAvailable = !exists;
+      loading = false;
+    }, 500);
+  }
+  async function confirmUsername () {}
 </script>
 
-<h2>Username</h2>
+<AuthCheck>
+  <h2>Username</h2>
+  <form class="w-2/5" on:submit|preventDefault={confirmUsername}>
+    <input 
+      type="text"
+      placeholder="Username"
+      class="input w-full"
+      bind:value={username}
+      on:input={checkAvailability}
+    />
+
+    <p>Is is available? {isAvailable}</p>
+
+    <button class="btn btn-success">Confirm username @{username} </button>
+  </form>
+</AuthCheck>
